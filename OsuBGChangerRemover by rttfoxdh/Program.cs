0b6faf335b9ff24883ecb010d151b6dfe7032ba0
rttfoxdh <rttfoxdh@DESKTOP-P7LG2MH>
2019-04-23 01:32:09 +0300

    23.04.19

---
+++ OsuBGChangerRemover by rttfoxdh/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace OsuBackgroundChanger
+{
+    class Program
+    {
+        static void Replace(string folderlocation)
+        {
+            string bgfile = "";
+            Console.WriteLine("Choose a background");
+            var filebrowser = new OpenFileDialog();
+            using (filebrowser)
+            {
+                if(filebrowser.ShowDialog() == DialogResult.OK)
+                {
+                    bgfile = filebrowser.FileName;
+                }
+            }
+            foreach (string folder in Directory.GetDirectories(folderlocation))
+            {
+                string foundedbg = "";
+                foreach (string file in Directory.GetFiles(folder))
+                {
+                    if (file.IndexOf(".osu")>-1)
+                    {
+                        int ifnothing=0;
+                        using (StreamReader reader = new StreamReader(file)){
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                if (line.IndexOf("//Background and Video events") > -1)
+                                {
+                                    line = reader.ReadLine();
+                                    foundedbg = line.Remove(0, 5);
+                                    foundedbg = foundedbg.Remove(foundedbg.Length - 5, 5);
+                                    if (foundedbg == "")
+                                        ifnothing = 1;
+                                    break;
+                                }
+                            }
+                        }
+                        if (ifnothing == 1)
+                        {
+                            foundedbg = bgfile.Remove(0, bgfile.LastIndexOf("\\")+1);
+                            string _text = File.ReadAllText(file);
+                            _text = _text.Replace("0,0,\"\",0,0", "0,0,\""+foundedbg+ "\",0,0");
+                            File.WriteAllText(file, _text);
+                            if (File.Exists(folder + "\\" + foundedbg) == true)
+                                File.Delete(folder + "\\" + foundedbg);
+                            File.Copy(bgfile, folder + "\\" + foundedbg);
+                        }
+                        else
+                        {
+                            string text = File.ReadAllText(file);
+                            text = text.Replace(foundedbg, foundedbg.Remove(foundedbg.IndexOf(".") + 1) + bgfile.Remove(0, bgfile.IndexOf(".") + 1));
+                            File.WriteAllText(file, text);
+                            if (foundedbg != "")
+                            {
+                                if (File.Exists(folder + "\\" + foundedbg) == true)
+                                    File.Delete(folder + "\\" + foundedbg);
+                                foundedbg = foundedbg.Remove(foundedbg.IndexOf(".") + 1) + bgfile.Remove(0, bgfile.IndexOf(".") + 1);
+                                File.Copy(bgfile, folder + "\\" + foundedbg);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        static void Delete(string folderlocation)
+        {
+            foreach (string folder in Directory.GetDirectories(folderlocation))
+            {
+                string foundedbg = "";
+                foreach (string file in Directory.GetFiles(folder))
+                {
+                    if (file.IndexOf(".osu") > -1)
+                    {
+                        using (StreamReader reader = new StreamReader(file))
+                        {
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                if (line.IndexOf("//Background and Video events") > -1)
+                                {
+                                    line = reader.ReadLine();
+                                    foundedbg = line.Remove(0, 5);
+                                    foundedbg = foundedbg.Remove(foundedbg.Length - 5, 5);
+                                    break;
+                                }
+                            }
+                        }
+                        string text = File.ReadAllText(file);
+                        text = text.Remove(text.IndexOf(foundedbg), foundedbg.Length);
+                        File.WriteAllText(file, text);
+                        if (foundedbg != "")
+                        {
+                            if (File.Exists(folder + "\\" + foundedbg) == true)
+                                File.Delete(folder + "\\" + foundedbg);
+                        }
+                    }
+                }
+            }
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Developer : https://github.com/rttfoxdh");
+            Console.WriteLine("Choose Songs location folder ( deffault: osu!\\Songs )");
+            string folderlocation="";
+            var folderbrowser = new FolderBrowserDialog();
+            using (folderbrowser)
+            {
+                if (folderbrowser.ShowDialog() == DialogResult.OK)
+                    folderlocation = folderbrowser.SelectedPath;
+            }
+            if (folderlocation == "")
+            {
+            }
+            else
+            {
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("1. Replace all backgrounds");
+                Console.WriteLine("2. Delete all backgrounds");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        Replace(folderlocation);
+                        break;
+                    case 2:
+                        Delete(folderlocation);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ OsuBGChangerRemover by rttfoxdh/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("OsuBackgroundChanger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OsuBackgroundChanger")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("003f9095-f95c-434c-b997-d3c5d87cc784")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
